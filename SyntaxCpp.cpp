723f7872b03587d47426ffbe8969273cbcbff7d0
Toster <128224128+Toster1132@users.noreply.github.com>
2025-01-25 12:14:55 +0100

    tylko syntax

---
+++ SyntaxCpp.cpp
@@ -0,0 +1,45 @@
+int przyklad = 0; // 0
+double przyklad = 0.0; // 0.0
+string przyklad = zero; // "zero"
+char przyklad = z // 'z'
+bool przyklad = false; // false
+
+/*
+Tylko syntax, nic kurde faja innego
+*/
+switch() {
+  case 1: case 2:
+  break;
+
+  case 3: case 4:
+  break;
+
+  default:
+  break;
+}
+
+
+
+if()
+
+  else if ()
+
+  else
+
+
+  
+while() {
+}
+
+
+
+for() {
+// () <- np, zmienna;zakres;iteracja
+}
+
+
+
+/*Jakieś funkcje, sklejasz akcje?*/
+
+sqrt() //pierwiastek kwadratowy
+square() //do kwadratu
